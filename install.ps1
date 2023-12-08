855af080875233137e1187be6a75724a2beb7382
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-08 18:58:27 +0800

    rm: unwanted

--- install.ps1
+++ install.ps1
@@ -1,9 +1,3 @@
-# $home_dir = $HOME
-# $profile_dir = 'Documents\WindowsPowerShell'
-# $profile_fname = 'Microsoft.PowerShell_profile.ps1'
-# $install_dir = "{0}\{1}\{2}" -f $home_dir, $profile_dir, $profile_fname
-# Write-Output $install_dir
-
 # create symbolic link from $PROFILE file to repo index.ps1
 New-Item -ItemType Symboliclink -Force `
     -Path $PROFILE `
