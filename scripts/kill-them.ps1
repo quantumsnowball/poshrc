522a942182b76d9e1f1ab0889113565d827a7e20
Quantum Snowball <quantum.snowball@gmail.com>
2024-04-29 20:36:49 +0800

    try catch for non-killable processes

--- scripts/kill-them.ps1
+++ scripts/kill-them.ps1
@@ -5,7 +5,12 @@ $processes = Get-Process
 foreach ($keyword in $keywords) {
     $matchingProcesses = $processes | Where-Object { $_.ProcessName -like "*$keyword*" }
     foreach ($process in $matchingProcesses) {
-        $process | Stop-Process -Force
-        Write-Host "Killed: $($process.ProcessName): $($process.Id)"
+        try {
+            $process | Stop-Process -Force -ErrorAction Stop
+            Write-Host "Killed: $($process.ProcessName): $($process.Id)"
+        }
+        catch {
+            Write-Host "Failed: $($process.ProcessName): $($process.Id)" -ForegroundColor Red
+        }
     }
 }
