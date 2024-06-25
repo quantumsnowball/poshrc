2c51130b25c71232c3b822e0c19fe1316aa3c9bb
Quantum Snowball <quantum.snowball@gmail.com>
2024-06-25 15:05:43 +0800

    gdu

--- utils/__init__.ps1
+++ utils/__init__.ps1
@@ -1,3 +1,4 @@
 . "$PSScriptRoot\shell.ps1"
 . "$PSScriptRoot\dir.ps1"
 . "$PSScriptRoot\zoxide.ps1"
+. "$PSScriptRoot\gdu.ps1"
---
+++ utils/gdu.ps1
@@ -0,0 +1,7 @@
+# installation:
+#   winget install gdu
+# setup:
+#   after installation, you get a gdu_windows_amd64.exe executable
+#   this is the binary gdu for windows
+# gdu
+Set-Alias -Name gdu -Value gdu_windows_amd64.exe
