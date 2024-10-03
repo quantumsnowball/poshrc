8b136052e968dd3060bfbe5bdfc1a081e51440b5
Quantum Snowball <quantum.snowball@gmail.com>
2024-10-03 20:31:36 +0800

    better const and msg

--- scripts/oculus/oculus-killer.ps1
+++ scripts/oculus/oculus-killer.ps1
@@ -5,9 +5,13 @@ param (
 
 # const
 $OCULUS_DASH_DIR = "C:\Program Files\Oculus\Support\oculus-dash\dash\bin"
-$OCULUS_ORIGINAL_DASH_SRC = "C:\Program Files\OculusKiller\OculusDash.exe.bak"
-$OCULUS_KILLER_DASH_SRC = "C:\Program Files\OculusKiller\OculusDash.exe"
-$OCULUS_DASH_SYMLINK = "C:\Program Files\Oculus\Support\oculus-dash\dash\bin\OculusDash.exe"
+$OCULUS_KILLER_DIR = "C:\Program Files\OculusKiller"
+$ORIGINAL_DASH_FNAME = "OculusOriginalDash.exe"
+$KILLER_DASH_FNAME = "OculusKillerDash.exe"
+$DASH_FNAME = "OculusDash.exe"
+$OCULUS_ORIGINAL_DASH_SRC = "$OCULUS_KILLER_DIR\$ORIGINAL_DASH_FNAME"
+$OCULUS_KILLER_DASH_SRC = "$OCULUS_KILLER_DIR\$KILLER_DASH_FNAME"
+$OCULUS_DASH_SYMLINK = "$OCULUS_DASH_DIR\$DASH_FNAME"
 
 # kill every process related
 kill-them.ps1 steam,oculus,ovr,vr,meta
@@ -20,11 +24,11 @@ switch ($Action) {
     "Enable" {
         # Create a symbolic link to the killer dash source
         New-Item -ItemType SymbolicLink -Path $OCULUS_DASH_SYMLINK -Target $OCULUS_KILLER_DASH_SRC
-        Write-Host "`nOculus Killer Dash Enabled: `n$OCULUS_DASH_SYMLINK`n -> $OCULUS_KILLER_DASH_SRC"
+        Write-Host "`nOculus Killer Dash Enabled: $DASH_FNAME -> $KILLER_DASH_FNAME`n"
     }
     "Restore" {
         # Create a symbolic link to the original dash source
         New-Item -ItemType SymbolicLink -Path $OCULUS_DASH_SYMLINK -Target $OCULUS_ORIGINAL_DASH_SRC
-        Write-Host "`nOculus Original Dash Restored: `n$OCULUS_DASH_SYMLINK`n -> $OCULUS_ORIGINAL_DASH_SRC"
+        Write-Host "`nOculus Original Dash Restored: $DASH_FNAME -> $ORIGINAL_DASH_FNAME`n"
     }
 }
