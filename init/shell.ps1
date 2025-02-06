a770e3c8cccc55cc66f60bbad02b173273532383
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-07 03:12:01 +0800

    shell command auto suggestions

--- init/shell.ps1
+++ init/shell.ps1
@@ -15,4 +15,11 @@ function OnViModeChange {
 Set-PSReadLineOption -EditMode Vi
 Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
 
+# shell command auto suggestions
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineOption -Colors @{ InlinePrediction = '#875f5f'}
+Set-PSReadLineKeyHandler -Chord "Ctrl+RightArrow" -Function ForwardWord
 
