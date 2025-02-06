a2cf8ec322379c04b9569e51a5bc3bb34042cbf6
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-07 02:50:57 +0800

    fzf ctrl+r ctrl+t, PSfzf wrapper

--- init/fzf.ps1
+++ init/fzf.ps1
@@ -1,1 +1,5 @@
 $env:FZF_DEFAULT_OPTS = "--height 40% --layout=reverse --border"
+
+# PSfzf
+# installation command: Install-Module -Name PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
