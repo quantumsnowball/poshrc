653b2e1b8952ad1a2c4f96d06dce5242477c0fc3
Quantum Snowball <quantum.snowball@gmail.com>
2025-11-26 07:56:04 +0800

    kill adb alvr

--- scripts/oculus/quit-vr.ps1
+++ scripts/oculus/quit-vr.ps1
@@ -3,7 +3,7 @@ Stop-Service "OVRService"
 Start-Sleep -Seconds 2
 
 # then kill all related apps
-kill-them steam,oculus,ovr,vr,meta
+kill-them steam,oculus,ovr,vr,meta,adb,alvr
 Start-Sleep -Seconds 5
 
 # switch to PC speaker
