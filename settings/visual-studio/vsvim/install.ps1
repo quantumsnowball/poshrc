dd177f6017f4042a0b6f7814610f2d03511f674f
Quantum Snowball <quantum.snowball@gmail.com>
2025-12-28 15:25:55 +0800

    fix: vsvimrc new path install script

--- settings/visual-studio/vsvim/install.ps1
+++ settings/visual-studio/vsvim/install.ps1
@@ -2,4 +2,4 @@
 # usage: run with admin right to create symlink
 New-Item -ItemType Symboliclink -Force `
     -Path "$HOME\.vsvimrc" `
-    -Target "\\wsl.localhost\Arch\home\$Env:USERNAME\.config\poshrc\settings\visual-studio\vsvim\vsvimrc"
+    -Target "$HOME\Documents\PowerShell\poshrc\settings\visual-studio\vsvim\vsvimrc"
