f2bdc8fcb7826a95c2257da94de4953698bd560b
Quantum Snowball <quantum.snowball@gmail.com>
2026-01-02 08:45:47 +0800

    popup.info helper

--- utils/shell.ps1
+++ utils/shell.ps1
@@ -10,3 +10,18 @@ Set-Alias -Name which -Value Get-Command
 
 # exit
 function x { Exit }
+
+# popup
+function popup.info {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Message,
+        [string]$Title = "System Notification"
+    )
+
+    # Ensure the GUI library is loaded
+    Add-Type -AssemblyName System.Windows.Forms
+    
+    # Display the box
+    [System.Windows.Forms.MessageBox]::Show($Message, $Title, "OK", "Information")
+}
