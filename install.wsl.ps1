cb75467349bc898aa9e4e5c1345132dea8e34552
Quantum Snowball <quantum.snowball@gmail.com>
2026-02-22 16:48:14 +0800

    comments, new names

--- install.wsl.ps1
+++ install.wsl.ps1
@@ -1,3 +1,5 @@
+# clone the repo to window host instead, this script is deprecated
+#
 # NOTE 
 # Execute this file using PowerShell v7 under the repo root directory
 # (in powershell, cd into the linux distro directory using file explorer link,
@@ -7,10 +9,10 @@
 # create symlink from $PROFILE file to repo __init__.ps1
 New-Item -ItemType Symboliclink -Force `
     -Path $PROFILE `
-    -Target "\\wsl.localhost\Arch\home\$Env:USERNAME\.config\poshrc\__init__.ps1"
+    -Target "\\wsl.localhost\ArchWSL\home\$Env:USERNAME\.config\poshrc\__init__.ps1"
 
 # create symlink inside $PROFILE's directory for this repo directory on the windows side 
 # this is needed for __init__.ps1 to be able to locate all other modules
 New-Item -ItemType Symboliclink -Force `
     -Path "$(Split-Path $PROFILE)\poshrc" `
-    -Target "\\wsl.localhost\Arch\home\$Env:USERNAME\.config\poshrc\"
+    -Target "\\wsl.localhost\ArchWSL\home\$Env:USERNAME\.config\poshrc\"
