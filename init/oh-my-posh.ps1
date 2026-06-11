6aa23c2577a623a7980f07669a1a7165e2733ab3
Quantum Snowball <quantum.snowball@gmail.com>
2026-06-12 02:33:30 +0800

    reorganize theme files into theme dir

--- init/oh-my-posh.ps1
+++ init/oh-my-posh.ps1
@@ -1,4 +1,5 @@
-$theme_path = "$PSScriptRoot\theme.json"
+$parent_dir = Split-Path -Parent $PSScriptRoot
+$theme_path = Join-Path $parent_dir "theme/theme.json"
 
 oh-my-posh init pwsh --config $theme_path | Invoke-Expression
 
